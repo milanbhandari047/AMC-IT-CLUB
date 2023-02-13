89c5dab7b36679243bb3cf15956b30bc06f41ed6
milanbhandari047@gmail.com <milanbhandari047@gmail.com>
2023-02-13 22:45:27 +0545

    change footer

--- src/Component/Common/Footer/Footer.jsx
+++ src/Component/Common/Footer/Footer.jsx
@@ -1,46 +1,67 @@
-import React from 'react'
-import"./Footer.css"
-import{BsFacebook,BsGithub} from "react-icons/bs"
+import React from "react";
+import "./Footer.css";
+import { BsFacebook, BsGithub } from "react-icons/bs";
 // import {MdEmail} from "react-icons/md"
-
-
 
 const Footer = () => {
   return (
     <>
-    
-<footer>
-  <div className="footer-content">
-    <h3> About Us  </h3>
-    <p>AMC IT CLUB is a student run club that provides a platform for student to learn , collaborate , and develop their skill in the field of information Technology.</p>
+      <footer>
+        <div className="footer-content">
+          <div className="footer_left">
+            <h3> About Us </h3>
+            <p>
+              AMC IT CLUB is a student run club that provides a platform for
+              student to learn , collaborate , and develop their skill in the
+              field of information Technology.
+            </p>
+          </div>
+          <div className="footer_middle">
+            <h3>Contact Us</h3>
+            <p>
+              Ghorahi 15,Ambikshwari campus
+              <br />
+              Email:amcitclub@gmail.com
+            </p>
+          </div>
 
-    <h3>Contact Us</h3>
-    <p>Ghorahi 15,Ambikshwari campus 
-      <br />
-      Email:amcitclub@gmail.com
-    </p>
+          <div className="footer_right">
+            <ul className="social-link">
+              <h3>Socail Media</h3>
+              <li>
+                <a href="https://www.facebook.com/amcitclub">
+                {" "}
+                  
+                  <i className="fa-brands fa-facebook">
+                    {" "}
+                    <BsFacebook />{" "}
+                  </i>{" "}
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/amcitclub">
+                  {" "}
+                  <i className="fa-brands fa-github">
+                    {" "}
+                    <BsGithub />{" "}
+                  </i>
+                  Github{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
 
-    <ul className='social-link' >
-    <li>
-                                        <a href="https://www.facebook.com/amcitclub"> <i className='fa-brands fa-facebook' > <BsFacebook/> </i> </a>
-                                    </li>
-                                    <li>
-                                        <a href="https://github.com/amcitclub"> <i className='fa-brands fa-github' >  <BsGithub/> </i> </a>
-                                    </li>
-                                   
-    </ul>
-  </div>
-  <div className="footer-bottom">
-    <p>copyright &copy;2023 AMCITCLUB. Designed by <span>Milan Bhandari</span></p>
-  </div>
-</footer>
-
+        <div className="footer-bottom">
+          <p>
+            copyright &copy;2023 AMCITCLUB. Designed by{" "}
+            <span>Milan Bhandari</span>
+          </p>
+        </div>
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
-
-
-
-                                    
+export default Footer;
